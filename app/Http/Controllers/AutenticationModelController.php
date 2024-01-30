4340fdbfa46d9404a361140253e5fe05b88e4df9
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-30 15:47:47 +0100

    Creazione del model

---
+++ app/Http/Controllers/AutenticationModelController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AutenticationModel;
+use App\Http\Requests\StoreAutenticationModelRequest;
+use App\Http\Requests\UpdateAutenticationModelRequest;
+
+class AutenticationModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAutenticationModelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AutenticationModel $autenticationModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AutenticationModel $autenticationModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAutenticationModelRequest $request, AutenticationModel $autenticationModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AutenticationModel $autenticationModel)
+    {
+        //
+    }
+}

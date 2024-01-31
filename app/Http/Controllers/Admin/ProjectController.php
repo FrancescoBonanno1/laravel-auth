312c516520af908c4a36a8eb2b6aa2f42f97b940
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-31 16:08:08 +0100

    add: destroy function

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-       $projects = Project::all();
+        $projects = Project::all();
         return view('admin.projects.index', compact('projects'));
     }
 
@@ -62,6 +62,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('admin.projects.index');
     }
 }

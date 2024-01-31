29a22f3eea6fff54f4f4975812664c2db0894f73
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-31 17:55:55 +0100

    Cambiamento stile su show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,11 +6,11 @@
     <div class="container">
         <h1 class="text-center"><strong>Dettagli su {{$project->name}}</strong></h1>
         <div class="row">
-                <div class="col-6">
-                    <div class="card mb-3" style="width: 90%;">
-                        <img src="{{ $project->image }}" class="card-img-top " alt="..." style=" height: 23rem;">
+                <div class="col-8">
+                    <div class="card mb-3" style="width: 40vw;">
+                        <img src="{{ $project->image }}"  class="img-fluid" alt="Responsive Image" style="height: 30%;">
                         <div id="card" class="card-body">
-                            <h5 class="card-title"><strong>{{ $project->name }} </strong></h5>
+                            <h5 class="card-title"><strong>{{$project->name}} </strong></h5>
                             <p class="card-text">{{ $project->description }}</p>
                             <p class="card-text"><strong>Data creazione</strong> {{ $project->dataCreation }}</p>
                             <p class="card-text"><strong>Language</strong> {{ $project->language }}</p>
